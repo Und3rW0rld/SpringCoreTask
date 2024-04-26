2026e056daf84de018bbdfeffdd72354cdb342e0
under_world <underworld.programmer@gmail.com>
2024-04-25 22:40:52 -0500

    First step of the project finished
    # Conflicts:
    #	.idea/misc.xml
    #	pom.xml
    #	src/main/java/com/uw/App.java
    #	src/main/java/com/uw/config/AppConfig.java
    #	src/main/java/com/uw/dao/TraineeDao.java
    #	src/main/java/com/uw/dao/TraineeDaoImpl.java
    #	src/main/java/com/uw/dao/TrainerDao.java
    #	src/main/java/com/uw/dao/TrainerDaoImpl.java
    #	src/main/java/com/uw/dao/TrainingDao.java
    #	src/main/java/com/uw/dao/TrainingDaoImpl.java
    #	src/main/java/com/uw/facade/Facade.java
    #	src/main/java/com/uw/model/Trainee.java
    #	src/main/java/com/uw/model/Trainer.java
    #	src/main/java/com/uw/model/Training.java
    #	src/main/java/com/uw/model/TrainingType.java
    #	src/main/java/com/uw/model/User.java
    #	src/main/java/com/uw/service/TraineeService.java
    #	src/main/java/com/uw/service/TraineeServiceImpl.java
    #	src/main/java/com/uw/service/TrainerService.java
    #	src/main/java/com/uw/service/TrainerServiceImpl.java
    #	src/main/java/com/uw/service/TrainingService.java
    #	src/main/java/com/uw/service/TrainingServiceImpl.java
    #	src/main/java/com/uw/util/PasswordGenerator.java
    #	src/main/java/com/uw/util/PasswordGeneratorImpl.java
    #	src/main/java/com/uw/util/UserNameGenerator.java
    #	src/main/resources/application.properties

---
+++ src/main/java/com/uw/util/StorageImpl.java
@@ -0,0 +1,50 @@
+package com.uw.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Scope("prototype")
+@Component
+public class StorageImpl implements Storage{
+
+    private  Map<Long, Object> myStorage;
+
+    private long size;
+
+    public StorageImpl(){
+        this.size = 0;
+    }
+
+    @Autowired
+    public void setMyStorage( Map<Long, Object> myStorage ) {
+        this.myStorage = myStorage;
+
+    }
+
+    @Override
+    public void save(Long key, Object value) {
+        size++;
+        myStorage.put(key, value);
+    }
+
+    @Override
+    public Object get(Long key) {
+        return myStorage.get(key);
+    }
+
+    @Override
+    public void remove(Long key) {
+        myStorage.remove(key);
+    }
+
+    public long getSize(){
+        return size;
+    }
+
+    public Map<Long, Object> getMyStorage() {
+        return myStorage;
+    }
+}
