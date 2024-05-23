4ade56d1940da96882271e6db2a2c3b117acf7c5
under_world <underworld.programmer@gmail.com>
2024-05-23 18:30:23 -0500

    Hibernate task done

--- src/main/java/com/uw/util/Storage.java
+++
@@ -1,8 +0,0 @@
-package com.uw.util;
-
-public interface Storage {
-    void save( Long key, Object value);
-    Object get(Long key);
-    void remove(Long key);
-    long getSize();
-}
--- src/test/java/com/uw/service/TrainingServiceTest.java
+++ src/test/java/com/uw/service/TrainingServiceTest.java
@@ -1,44 +1,76 @@
 package com.uw.service;
 
-import com.uw.dao.TrainingDao;
 import com.uw.dao.TrainingDaoImpl;
-import com.uw.model.Training;
-import com.uw.model.TrainingType;
-import com.uw.util.Storage;
-import com.uw.util.StorageImpl;
+import com.uw.model.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
-import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
 public class TrainingServiceTest {
 
-    private Storage storage = new StorageImpl();
-    private TrainingService trainingService = new TrainingServiceImpl();
-    private TrainingDao trainingDao = new TrainingDaoImpl();
+    SessionFactory sessionFactory;
+    private final TrainingService trainingService = new TrainingServiceImpl();
+
+    @After
+    public void tearDown() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
+    }
 
     @Before
     public void setUp() {
-        ((StorageImpl) storage).setMyStorage(new HashMap<>());
-        ((TrainingDaoImpl) trainingDao).setStorage(storage);
+        sessionFactory = new MetadataSources(new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build())
+                .buildMetadata()
+                .buildSessionFactory();
+        TrainingDaoImpl trainingDao = new TrainingDaoImpl();
+        trainingDao.setSessionFactory(sessionFactory);
         ((TrainingServiceImpl) trainingService).setTrainingDao(trainingDao);
+    }
+
+    private static Training getTraining(LocalDate dateOfBirth) {
+        Trainee trainee = new Trainee(dateOfBirth, "some address");
+        String name = "theName";
+        String lastName = "theLastName";
+        String username = name+"."+lastName;
+        String password = "0123456789";
+        User user = new User(name, lastName, username, password, true);
+        trainee.setUser(user);
+        long trainerId = 1L;
+
+        Trainer trainer = new Trainer();
+        TrainingType trainingType = new TrainingType();
+        trainingType.setTypeName("YOGA");
+        trainer.setSpecialization(trainingType);
+        trainer.setUser( user );
+
+        String trainingName = "Java Basics";
+        trainingType = new TrainingType();
+        trainingType.setTypeName("Workshop");
+        LocalDate trainingDate = LocalDate.of(2024, 5, 1);
+        int trainingDuration = 120;
+
+        // Crear un objeto de capacitación
+        Training training = new Training(trainingName, trainingType, trainingDate, trainingDuration);
+
+        training.setTrainee(trainee);
+        training.setTrainer(trainer);
+        return training;
     }
 
     @Test
     public void testCreateTraining() throws Exception {
         // Inicializar datos de la capacitación
-        long traineeId = 1L;
-        long trainerId = 1L;
-        String trainingName = "Java Basics";
-        TrainingType trainingType = new TrainingType("Workshop");
-        LocalDate trainingDate = LocalDate.of(2024, 5, 1);
-        String trainingDuration = "2 hours";
+        LocalDate dateOfBirth = LocalDate.of(2000, 2, 12);
+        Training training = getTraining(dateOfBirth);
 
-        // Crear un objeto de capacitación
-        Training training = new Training(traineeId, trainerId, trainingName, trainingType, trainingDate, trainingDuration);
 
         // Crear la capacitación en el servicio y obtener el ID
         long id = trainingService.createTraining(training);
@@ -50,20 +82,22 @@ public class TrainingServiceTest {
         assertNotNull(training);
 
         // Verificar que la capacitación se pueda recuperar del servicio y sea igual a la creada
-        assertEquals(training, trainingService.selectTrainingProfile(id));
+        assertEquals(training.toString(), trainingService.selectTrainingProfile(id).toString());
     }
+
+
 
     @Test
     public void testSelectTrainingProfile_Success() throws Exception {
         // Crear una capacitación
-        long traineeId = 1L;
-        long trainerId = 1L;
         String trainingName = "Java Basics";
-        TrainingType trainingType = new TrainingType("Workshop");
+        TrainingType trainingType = new TrainingType();
+        trainingType.setTypeName("Workshop");
         LocalDate trainingDate = LocalDate.of(2024, 5, 1);
         String trainingDuration = "2 hours";
 
-        Training training = new Training(traineeId, trainerId, trainingName, trainingType, trainingDate, trainingDuration);
+        LocalDate dateOfBirth = LocalDate.of(2000, 2, 12);
+        Training training = getTraining(dateOfBirth);
 
         // Crear la capacitación en el servicio y obtener el ID
         long id = trainingService.createTraining(training);
@@ -72,7 +106,7 @@ public class TrainingServiceTest {
         Training selectedTraining = trainingService.selectTrainingProfile(id);
 
         // Verificar que la capacitación seleccionada sea igual a la capacitación creada
-        assertEquals(training, selectedTraining);
+        assertEquals(training.toString(), selectedTraining.toString());
     }
 
     @Test(expected = Exception.class)
